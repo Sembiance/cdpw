2e954a4f4cddd02bb2d3364b56741f05bd66263e
Robert Schultz <robert@cosmicrealms.com>
2019-03-23 20:17:49 -0400

    Added captureScreenshot static method

--- CDPW.js
+++ CDPW.js
@@ -615,6 +615,42 @@ const XU = require("@sembiance/xu"),
 				cb
 			);
 		}
+
+		// Capture a screenshot
+		static captureScreenshot(url, _options, _cb)
+		{
+			const options = _cb ? _options : {};
+			const cb = (_cb || _options);
+
+			const cdpw = new CDPW(true, (cdpwErr, client) =>
+			{
+				if(cdpwErr)
+					return cb(cdpwErr);
+				
+				tiptoe(
+					function openSite()
+					{
+						cdpw.openURL(url, this, {delay : (options.delay || XU.SECOND), width : (options.width || 1280), height : (options.height || 1024)});
+					},
+					function capture()
+					{
+						client.Page.captureScreenshot({format : "png"}, this);
+					},
+					function cleanup(ss)
+					{
+						this.data.ssData = Buffer.from(ss.data, "base64");
+						cdpw.destroy(this);
+					},
+					function resturnResult(err)
+					{
+						if(err)
+							return cb(err);
+
+						cb(undefined, this.data.ssData);
+					}
+				);
+			});
+		}
 	}
 
 	module.exports = CDPW;
---
+++ test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const path = require("path"),
+	fs = require("fs"),
+	fileUtil = require("@sembiance/xutil").file,
+	CDPW = require("./CDPW.js");
+
+const SCREENSHOT_PATH = path.join(__dirname, "web-screenshot.png");
+
+if(fileUtil.existsSync(SCREENSHOT_PATH))
+	fs.unlinkSync(SCREENSHOT_PATH);
+
+CDPW.captureScreenshot("http://worldofsolitaire.com", {width : 1024, height : 768, delay : 2000}, (err, ss) =>
+{
+	if(err)
+	{
+		console.error(err);
+		process.exit(1);
+	}
+
+	fs.writeFile(SCREENSHOT_PATH, ss, () =>
+	{
+		console.log("Examine %s to see if it worked, should be a screenshot of worldofsolitaire.com", path.basename(SCREENSHOT_PATH));
+		process.exit(0);
+	});
+});
